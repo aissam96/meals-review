f96270ed06ac94c80dec8bb5756e438054b14d44
Aissam Erradouane <erradouane.aissam96@gmail.com>
2018-04-23 16:48:02 +0100

    Project Created

--- Meals Review/ViewController.swift
+++ Meals Review/ViewController.swift
@@ -13,13 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Project Created")
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
